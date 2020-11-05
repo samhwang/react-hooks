d314bf4a40e5b23aede7cee41b443fcad3cf2d95
Sam Huynh <samhwang2112.dev@gmail.com>
2020-11-06 10:03:06 +1100

    Add hook and demo component

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect, useCallback } from 'react';
+
+interface WindowSizeType {
+    width: number;
+    height: number;
+}
+
+function getSizeCallback(isClient: boolean) {
+  if (!isClient) {
+    return {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+}
+
+function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  const getSize = useCallback(getSizeCallback, [isClient]);
+
+  const [windowSize, setWindowSize] = useState<WindowSizeType>(getSize(isClient));
+
+  useEffect(() => {
+    function handleResize() {
+      if (!isClient) {
+        return;
+      }
+
+      const size = getSize(isClient);
+      setWindowSize(size);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isClient, getSize]);
+
+  return windowSize;
+}
+
+export default useWindowSize;
